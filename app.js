19a1d7f8ad286643fd4830fc9c588e6f4e13f08a
Ryan J Blake <ryanjblake1988@gmail.com>
2021-12-12 15:13:08 -0800

    feature

--- app.js
+++ app.js
@@ -1,1 +1,12 @@
-console.log('Hello Node!')
+const profileDataArgs = process.argv.slice(2, process.argv.length);
+console.log(profileDataArgs);
+
+const printProfileData = (profileDataArr) => {
+    for (let i = 0; i < profileDataArr.length; i += 1) {
+        console.log(profileDataArr[i]);
+    }
+
+    console.log('================');
+
+    profileDataArr.forEach(prfileItem => console.log(profileItem));
+};
